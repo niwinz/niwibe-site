b8853fef2ab4378b6d938a34ada9b072f5982ad2
Andrey Antukh <niwi@niwi.be>
2015-03-07 19:09:11 +0100

    Revert "Remove whitespace on pelican config."
    This reverts commit 7de7cd42e8ae79f40c3c8044df3d42fc5958c61e.

--- publishconf.py
+++ publishconf.py
@@ -6,10 +6,11 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://www.niwi.be'
+SITEURL = 'http://www.niwi.be'
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+
 
 DELETE_OUTPUT_DIRECTORY = True
 RELATIVE_URLS = False
