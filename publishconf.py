8b1b78f2ae167cdfa576f812927c9dd1e574a378
Andrey Antukh <niwi@niwi.be>
2015-03-07 19:07:59 +0100

    Revert "Change main url for publish config."
    This reverts commit 06562afb881094aad3c6d7a456549a90f57852ed.

--- publishconf.py
+++ publishconf.py
@@ -6,7 +6,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = '//www.niwi.be'
+SITEURL = 'https://www.niwi.be'
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
